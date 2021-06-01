0e187964fefe7532b49d360a28ebda235da76efc
SimoneCiferri <simoneciferri0112@gmail.com>
2021-06-01 13:05:16 +0200

    Game logic

--- app/src/main/java/it/ciferricaporro/memorygame/fragments/MemoryGame.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/fragments/MemoryGame.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.core.view.isVisible
 import it.ciferricaporro.memorygame.MainActivity.Companion.binding
 import it.ciferricaporro.memorygame.R
