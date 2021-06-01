d35f0fa897dbb8da340cf590e7eb55d121702eae
Giulio <caporrogiulio@gmail.com>
2021-06-01 15:53:49 +0200

    first part room

---
+++ app/src/main/java/it/ciferricaporro/memorygame/data/User.kt
@@ -0,0 +1,16 @@
+package it.ciferricaporro.memorygame.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Time
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val player: String,
+    val time: Double,
+    val Data: Time,
+    val errors: Int
+)
+
---
+++ app/src/main/java/it/ciferricaporro/memorygame/data/UserViewModel.kt
@@ -0,0 +1,26 @@
+package it.ciferricaporro.memorygame.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+    private val userDao = UserDatabase.getDatabase(application).userDao()
+
+    init{
+        readAllData = userDao.readAllData()
+    }
+
+    fun addUser(user: User){
+        viewModelScope.launch(Dispatchers.IO) {
+        userDao.addUser(user)
+
+        }
+    }
+
+}
