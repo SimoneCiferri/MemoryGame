65cb4291c27c54a8f21c8367a44fedea8d82f97d
Giulio <caporrogiulio@gmail.com>
2021-06-03 19:10:40 +0200

    Data attuale

--- app/src/main/java/it/ciferricaporro/memorygame/MainActivity.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/MainActivity.kt
@@ -2,6 +2,8 @@ package it.ciferricaporro.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,6 +11,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import it.ciferricaporro.memorygame.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -19,6 +24,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setBottomNav()
+
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        Toast.makeText(this, " C DATE is $currentDate", Toast.LENGTH_LONG).show()
     }
 
     private fun setBottomNav(){
