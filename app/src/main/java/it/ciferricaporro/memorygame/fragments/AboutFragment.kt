6b0e0fe97b0844890f6fccc7d5cf906cb8ac256e
SimoneCiferri <simoneciferri0112@gmail.com>
2021-06-07 19:40:00 +0200

    About page

--- app/src/main/java/it/ciferricaporro/memorygame/fragments/AboutFragment.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/fragments/AboutFragment.kt
@@ -1,10 +1,13 @@
 package it.ciferricaporro.memorygame.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import it.ciferricaporro.memorygame.R
 
 
@@ -17,6 +20,13 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val viewA = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val tvLink = viewA.findViewById<TextView>(R.id.tvGitLink)
+        tvLink.setOnClickListener {
+            val i = Intent(android.content.Intent.ACTION_VIEW)
+            i.data = Uri.parse(tvLink.text.toString())
+            startActivity(i)
+        }
 
         return viewA
     }
