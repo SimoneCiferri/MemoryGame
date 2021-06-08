d3cd66007729469bb90fe34fff6a02e37cbe29e2
SimoneCiferri <simoneciferri0112@gmail.com>
2021-06-08 18:49:29 +0200

    tutorial

--- app/src/main/java/it/ciferricaporro/memorygame/viewPager/screens/FirstScreen.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/viewPager/screens/FirstScreen.kt
@@ -1,10 +1,12 @@
 package it.ciferricaporro.memorygame.viewPager.screens
 
+import android.animation.AnimatorInflater
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.viewpager2.widget.ViewPager2
 import it.ciferricaporro.memorygame.R
@@ -17,6 +19,11 @@ class FirstScreen : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val viewF = inflater.inflate(R.layout.fragment_first_screen, container, false)
+        val fing = viewF.findViewById<ImageView>(R.id.ivFinger)
+        val zoom = AnimatorInflater.loadAnimator(requireContext(), R.animator.zoom)
+        zoom.setTarget(fing)
+        zoom.duration = 800
+        zoom.start()
 
         val viewP = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
