470d5db70befe80be9f12b77f01f8215c7a8f156
SimoneCiferri <simoneciferri0112@gmail.com>
2021-07-05 15:50:57 +0200

    binding

--- app/src/main/java/it/ciferricaporro/memorygame/viewPager/ViewPagerFragment.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/viewPager/ViewPagerFragment.kt
@@ -7,22 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
 import it.ciferricaporro.memorygame.R
+import it.ciferricaporro.memorygame.databinding.FragmentViewPagerBinding
 import it.ciferricaporro.memorygame.viewPager.screens.FirstScreen
 import it.ciferricaporro.memorygame.viewPager.screens.SecondScreen
 import it.ciferricaporro.memorygame.viewPager.screens.ThirdScreen
 
 class ViewPagerFragment : Fragment() {
 
+    private lateinit var binding: FragmentViewPagerBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val viewPF = inflater.inflate(R.layout.fragment_view_pager, container, false)
+        //val viewPF = inflater.inflate(R.layout.fragment_view_pager, container, false)
+        binding = FragmentViewPagerBinding.inflate(layoutInflater, container, false)
 
         val fragmentList = arrayListOf<Fragment>( FirstScreen(), SecondScreen(), ThirdScreen() )
 
         val adapter = ViewPagerAdapter(fragmentList, requireActivity().supportFragmentManager, lifecycle)
 
-        viewPF.findViewById<ViewPager2>(R.id.viewPager).adapter = adapter
-        return viewPF
+        //viewPF.findViewById<ViewPager2>(R.id.viewPager).adapter = adapter
+        binding.viewPager.adapter = adapter
+
+        return binding.root
+        //return viewPF
     }
 }
