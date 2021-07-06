b5bb298d46bc08ca665e6057f827615df555f691
Giulio <caporrogiulio@gmail.com>
2021-07-06 16:15:56 +0200

    trophy

--- app/src/main/java/it/ciferricaporro/memorygame/fragments/MemoryGame.kt
+++ app/src/main/java/it/ciferricaporro/memorygame/fragments/MemoryGame.kt
@@ -205,7 +205,7 @@ class MemoryGame : Fragment() {
     }
 
     private fun newGame(){
-        images.shuffle()
+        //images.shuffle()
         cards = buttons.indices.map { index ->
             Card(images[index])
         }
